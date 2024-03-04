b000833d43c371ff3d76da6c8a7b2a0350077497
Sebastian B <sberesinski@gmail.com>
2024-03-04 14:09:46 +0000

    version 1.0.0

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
